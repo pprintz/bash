8144b37cd6538d50e99f33d11a8d0837a33c3606
Eduardo Shiota Yasuda <contato@eduardosy.com.br>
2011-11-24 12:41:04 -0200

    Turns out I'm a bash n00b, and should've used the correct /Users/eshiota variable

--- scripts/other.sh
+++ scripts/other.sh
@@ -1,1 +1,1 @@
-[[ -s "~/.rvm/scripts/rvm" ]] && source "~/.rvm/scripts/rvm"  # This loads RVM into a shell session.
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
