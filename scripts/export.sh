8290993f1f267a984b54935505ff36c485ac719c
Eduardo Shiota Yasuda <yl.edu.sy@gmail.com>
2013-10-22 22:57:19 -0200

    Add .bash bin path

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,4 +1,4 @@
-export PATH="/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/mysql/bin:/usr/local/git/bin:~/bin:/Users/eshiota/.cabal/bin:$PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/mysql/bin:/usr/local/git/bin:~/bin:/Users/eshiota/.cabal/bin:~/.bash/bin:$PATH"
 export MANPATH=/opt/local/share/man:$MANPATH
 export SVN_EDITOR=vim
 export RUBYOPT=rubygems
